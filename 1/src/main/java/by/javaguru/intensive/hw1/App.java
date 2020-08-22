c5a9f58c2b43fc0bffcbd13999e98018a8ddeb83
Alexey Kashlyak <lexa.april@gmail.com>
2020-08-22 22:17:42 +0300

    Test

--- 1/src/main/java/by/javaguru/intensive/hw1/App.java
+++
@@ -1,4 +0,0 @@
-package by.javaguru.intensive.hw1;
-
-public class App {
-}
---
+++ Circle/src/main/java/by/javaguru/intensive/hw/App.java
@@ -0,0 +1,8 @@
+package by.javaguru.intensive.hw;
+
+public class App {
+    public static void main(String[] args) {
+        //        entry point, to be implemented
+
+    }
+}
